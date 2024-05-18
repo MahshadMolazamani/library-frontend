29ba84d6f637be6cba0017309f2c8820aa2c0bb8
Mahshad Mollazamani <mmolazamani@gmail.com>
2024-05-18 22:47:40 +0200

    Adding SearchBook Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
-import { Footer } from "./layouts/NavbarAndFooter/Footer";
-import { HomePage } from "./layouts/HomePage/HomePage";
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
+import {Footer} from "./layouts/NavbarAndFooter/Footer";
+import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
 
 export const App = () => {
-  return (
-      <>
-        <Navbar />
-        <HomePage />
-        <Footer />
-      </>
-  );
+    return (
+        <>
+            <Navbar/>
+            {/*<HomePage/>*/}
+            <SearchBooksPage/>
+            <Footer/>
+        </>
+    );
 }

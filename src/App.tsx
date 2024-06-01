4e9438e062433737f7c05b9db2fe88e811130e2f
Mahshad Mollazamani <mmolazamani@gmail.com>
2024-06-01 10:35:03 +0200

    adding Switch

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,27 @@ import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
 import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
 import {HomePage} from './layouts/HomePage/HomePage';
-import {Route} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 
 export const App = () => {
     return (
         <>
             <Navbar/>
-            <Route path='/' exact>
-                <HomePage/>
-            </Route>
+            <Switch>
 
-            <Route path='/search'>
-                <SearchBooksPage/>
-            </Route>
+                <Route path='/' exact>
+                    <Redirect to='/home'/>
+                </Route>
+
+                <Route path='/home'>
+                    <HomePage/>
+                </Route>
+
+                <Route path='/search'>
+                    <SearchBooksPage/>
+                </Route>
+
+            </Switch>
 
             <Footer/>
         </>
